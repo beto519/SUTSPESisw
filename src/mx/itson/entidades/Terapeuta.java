d0d38046bc37f74cd8cd0c51e6d346e43b656ae0
Jesus Alberto Rivera Lopez <jesusalbertoriveral205530@gmail.com>
2022-04-04 14:28:38 -0500

    beto519
    Creación de la clase de terapeuta

--- src/mx/itson/entidades/Terapeuta.java
+++ src/mx/itson/entidades/Terapeuta.java
@@ -9,5 +9,83 @@ package mx.itson.entidades;
  * @author vinko
  */
 public class Terapeuta {
+    private int id;
+    private String nombre;
+    private String codigoUsuario;
+    private String contraseña;
+    private String puesto;
+
+    public Terapeuta() {
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the codigoUsuario
+     */
+    public String getCodigoUsuario() {
+        return codigoUsuario;
+    }
+
+    /**
+     * @param codigoUsuario the codigoUsuario to set
+     */
+    public void setCodigoUsuario(String codigoUsuario) {
+        this.codigoUsuario = codigoUsuario;
+    }
+
+    /**
+     * @return the contraseña
+     */
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    /**
+     * @param contraseña the contraseña to set
+     */
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    /**
+     * @return the puesto
+     */
+    public String getPuesto() {
+        return puesto;
+    }
+
+    /**
+     * @param puesto the puesto to set
+     */
+    public void setPuesto(String puesto) {
+        this.puesto = puesto;
+    }
     
 }
