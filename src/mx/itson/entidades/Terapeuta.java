290342e2668528601f702e04d9c86d426fcf1bb9
VINKO-NEVESCANIN <nevescanin.vinko@gmail.com>
2022-04-04 15:06:55 -0700

    VINKO
    En el paquete entidades cree las clases Alumno, Programa, Terapeuta con sus respetivas variables.

--- src/mx/itson/entidades/Terapeuta.java
+++ src/mx/itson/entidades/Terapeuta.java
@@ -10,4 +10,50 @@ package mx.itson.entidades;
  */
 public class Terapeuta {
     
+    private int idTerapeuta;
+    private String Nombre;
+    private String Area;
+    private String Email;
+    private String pasword;
+
+    public int getIdTerapeuta() {
+        return idTerapeuta;
+    }
+
+    public void setIdTerapeuta(int idTerapeuta) {
+        this.idTerapeuta = idTerapeuta;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getArea() {
+        return Area;
+    }
+
+    public void setArea(String Area) {
+        this.Area = Area;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    public String getPasword() {
+        return pasword;
+    }
+
+    public void setPasword(String pasword) {
+        this.pasword = pasword;
+    }
+    
 }
