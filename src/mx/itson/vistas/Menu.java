1d4052c46f7289c2c3577e92ec86260c12084494
VINKO-NEVESCANIN <nevescanin.vinko@gmail.com>
2022-04-06 12:39:01 -0700

    VINKO
    La funcionalidad principal del menu ya esta faltando ajustes de diseno.

--- src/mx/itson/vistas/Menu.java
+++ src/mx/itson/vistas/Menu.java
@@ -15,6 +15,7 @@ public class Menu extends javax.swing.JFrame {
      */
     public Menu() {
         initComponents();
+        
     }
 
     /**
@@ -26,14 +27,118 @@ public class Menu extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
+        btnAgregarTerapeuta = new javax.swing.JLabel();
+        btnEditarTerapeuta = new javax.swing.JLabel();
+        btnIniciarSesion = new javax.swing.JLabel();
+        btnIntrucciones = new javax.swing.JLabel();
+        btnVerNinos = new javax.swing.JLabel();
+        btnVerTerapeuta = new javax.swing.JLabel();
+        TxtTitulo = new javax.swing.JLabel();
         LabelFondo = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
-        getContentPane().add(LabelFondo, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 790, 620));
+
+        jPanel1.setBackground(new java.awt.Color(155, 20, 136));
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        btnAgregarTerapeuta.setIcon(new javax.swing.ImageIcon(getClass().getResource("/mx/itson/imagenes/AgregarTerapeuta.png"))); // NOI18N
+        btnAgregarTerapeuta.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                btnAgregarTerapeutaMouseClicked(evt);
+            }
+        });
+        jPanel1.add(btnAgregarTerapeuta, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 130, -1, -1));
+
+        btnEditarTerapeuta.setIcon(new javax.swing.ImageIcon(getClass().getResource("/mx/itson/imagenes/EditarTerapeuta.png"))); // NOI18N
+        btnEditarTerapeuta.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                btnEditarTerapeutaMouseClicked(evt);
+            }
+        });
+        jPanel1.add(btnEditarTerapeuta, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 250, -1, -1));
+
+        btnIniciarSesion.setIcon(new javax.swing.ImageIcon(getClass().getResource("/mx/itson/imagenes/log-in_1.png"))); // NOI18N
+        btnIniciarSesion.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                btnIniciarSesionMouseClicked(evt);
+            }
+        });
+        jPanel1.add(btnIniciarSesion, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 10, 100, 100));
+
+        btnIntrucciones.setIcon(new javax.swing.ImageIcon(getClass().getResource("/mx/itson/imagenes/IntruccionesBtn.png"))); // NOI18N
+        btnIntrucciones.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                btnIntruccionesMouseClicked(evt);
+            }
+        });
+        jPanel1.add(btnIntrucciones, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 370, -1, -1));
+
+        btnVerNinos.setIcon(new javax.swing.ImageIcon(getClass().getResource("/mx/itson/imagenes/editarNiño.png"))); // NOI18N
+        btnVerNinos.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                btnVerNinosMouseClicked(evt);
+            }
+        });
+        jPanel1.add(btnVerNinos, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 470, -1, -1));
+
+        btnVerTerapeuta.setIcon(new javax.swing.ImageIcon(getClass().getResource("/mx/itson/imagenes/contrato.png"))); // NOI18N
+        btnVerTerapeuta.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                btnVerTerapeutaMouseClicked(evt);
+            }
+        });
+        jPanel1.add(btnVerTerapeuta, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 560, -1, -1));
+
+        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 240, 670));
+
+        TxtTitulo.setFont(new java.awt.Font("Tempus Sans ITC", 3, 48)); // NOI18N
+        TxtTitulo.setForeground(new java.awt.Color(0, 0, 0));
+        TxtTitulo.setText("SUTSPESISW");
+        getContentPane().add(TxtTitulo, new org.netbeans.lib.awtextra.AbsoluteConstraints(660, 10, 290, -1));
+
+        LabelFondo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/mx/itson/imagenes/FondoLogin.jpg"))); // NOI18N
+        getContentPane().add(LabelFondo, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 950, 670));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void btnAgregarTerapeutaMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnAgregarTerapeutaMouseClicked
+        AgregarTerapeuta AgreTerapeuta = new AgregarTerapeuta();
+        AgreTerapeuta.setVisible(true);
+        dispose();
+    }//GEN-LAST:event_btnAgregarTerapeutaMouseClicked
+
+    private void btnEditarTerapeutaMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnEditarTerapeutaMouseClicked
+        EditarTerapeuta EdiTerapeuta = new EditarTerapeuta();
+        EdiTerapeuta.setVisible(true);
+        dispose();
+    }//GEN-LAST:event_btnEditarTerapeutaMouseClicked
+
+    private void btnIniciarSesionMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnIniciarSesionMouseClicked
+        IniciarSesion logIn = new IniciarSesion();
+        logIn.setVisible(true);
+        dispose();
+    }//GEN-LAST:event_btnIniciarSesionMouseClicked
+
+    private void btnIntruccionesMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnIntruccionesMouseClicked
+        Instrucciones instruc = new Instrucciones();
+        instruc.setVisible(true);
+        dispose();
+    }//GEN-LAST:event_btnIntruccionesMouseClicked
+
+    private void btnVerNinosMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnVerNinosMouseClicked
+        VerNiños verNinino = new VerNiños();
+        verNinino.setVisible(true);
+        dispose();
+    }//GEN-LAST:event_btnVerNinosMouseClicked
+
+    private void btnVerTerapeutaMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnVerTerapeutaMouseClicked
+        VerTerapeutas verTerapeuta = new VerTerapeutas();
+        verTerapeuta.setVisible(true);
+        dispose();
+    }//GEN-LAST:event_btnVerTerapeutaMouseClicked
 
     /**
      * @param args the command line arguments
@@ -72,5 +177,13 @@ public class Menu extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel LabelFondo;
+    private javax.swing.JLabel TxtTitulo;
+    private javax.swing.JLabel btnAgregarTerapeuta;
+    private javax.swing.JLabel btnEditarTerapeuta;
+    private javax.swing.JLabel btnIniciarSesion;
+    private javax.swing.JLabel btnIntrucciones;
+    private javax.swing.JLabel btnVerNinos;
+    private javax.swing.JLabel btnVerTerapeuta;
+    private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
