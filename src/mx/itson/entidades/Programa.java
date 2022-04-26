9266c34ae083df033a08c3f38568767aa537e723
VINKO-NEVESCANIN <nevescanin.vinko@gmail.com>
2022-04-25 22:19:05 -0700

    Clase programa Actualizada
    Aparte tambien edicion base de las pestanas por nivel.

--- src/mx/itson/entidades/Programa.java
+++ src/mx/itson/entidades/Programa.java
@@ -11,6 +11,10 @@ package mx.itson.entidades;
 public class Programa {
     
     private int idNivel;
+    private int Nivel;
+    private String nombreNivel;
+    private String estadoPorcentaje;
+    private int idNiño;
     private String NombrePrograma;
 
     public int getIdNivel() {
@@ -27,5 +31,37 @@ public class Programa {
 
     public void setNombrePrograma(String NombrePrograma) {
         this.NombrePrograma = NombrePrograma;
+    }
+
+    public int getNivel() {
+        return Nivel;
+    }
+
+    public void setNivel(int Nivel) {
+        this.Nivel = Nivel;
+    }
+
+    public String getNombreNivel() {
+        return nombreNivel;
+    }
+
+    public void setNombreNivel(String nombreNivel) {
+        this.nombreNivel = nombreNivel;
+    }
+
+    public String getEstadoPorcentaje() {
+        return estadoPorcentaje;
+    }
+
+    public void setEstadoPorcentaje(String estadoPorcentaje) {
+        this.estadoPorcentaje = estadoPorcentaje;
+    }
+
+    public int getIdNiño() {
+        return idNiño;
+    }
+
+    public void setIdNiño(int idNiño) {
+        this.idNiño = idNiño;
     }
 }
